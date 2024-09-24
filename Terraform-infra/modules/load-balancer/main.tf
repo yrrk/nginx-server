91f95a5fb6b79e34be4569f1cfe7f06045be17f4
yrrk <kinyakrr@gmail.com>
2024-09-24 02:45:43 -0400

    configured public ip for lb

--- Terraform-infra/modules/load-balancer/main.tf
+++ Terraform-infra/modules/load-balancer/main.tf
@@ -15,7 +15,7 @@ resource "azurerm_lb" "load-balancer" {
 
     content {
       name                 = "PublicIPAddress"
-      public_ip_address_id = var.public-ip
+      public_ip_address_id = azurerm_public_ip.public-ip.id
     }
   }
   dynamic "frontend_ip_configuration" {
