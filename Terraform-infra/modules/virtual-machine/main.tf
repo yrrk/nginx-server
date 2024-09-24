789a5a887255346a6aa3150edab607672d0ee060
yrrk <kinyakrr@gmail.com>
2024-09-24 03:11:07 -0400

    deploying vms

--- Terraform-infra/modules/virtual-machine/main.tf
+++ Terraform-infra/modules/virtual-machine/main.tf
@@ -10,27 +10,27 @@ resource "azurerm_network_interface" "nic-card" {
   }
 }
 
-# resource "azurerm_linux_virtual_machine" "vm-deployment" {
-#   name                            = var.vm-name
-#   resource_group_name             = var.resource-group
-#   location                        = var.location
-#   size                            = "Standard_B1s"
-#   admin_username                  = var.username
-#   admin_password                  = var.password
-#   disable_password_authentication = false
-#   network_interface_ids = [
-#     azurerm_network_interface.nic-card.id,
-#   ]
+resource "azurerm_linux_virtual_machine" "vm-deployment" {
+  name                            = var.vm-name
+  resource_group_name             = var.resource-group
+  location                        = var.location
+  size                            = "Standard_B1s"
+  admin_username                  = var.username
+  admin_password                  = var.password
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.nic-card.id,
+  ]
 
-#   os_disk {
-#     caching              = "ReadWrite"
-#     storage_account_type = "Standard_LRS"
-#   }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
 
-#   source_image_reference {
-#     publisher = "Canonical"
-#     offer     = "0001-com-ubuntu-server-jammy"
-#     sku       = "22_04-lts"
-#     version   = "latest"
-#   }
-# }
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
